5a802822e20c7d909a848116891f947ef4bae20b
MinadukiSekina <taroufujimoto075@gmail.com>
2025-06-05 09:02:21 +0000

    feat: add db initialization in main roputine

--- main.go
+++ main.go
@@ -1,8 +1,15 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/MinadukiSekina/todo-go-app/app/db"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
+
+	// DBの初期化
+	db.Init()
+
 	router := gin.Default()
 
 	router.LoadHTMLGlob("templates/*/*.html")
