092cd0b0be05fa3a48123fdc8c0b99bcbbf41031
MinadukiSekina <taroufujimoto075@gmail.com>
2025-06-06 07:20:33 +0000

    feat: implement todoUsecase

--- app/db/todoRepository.go
+++ app/db/todoRepository.go
@@ -5,38 +5,38 @@ import (
 	"github.com/MinadukiSekina/todo-go-app/app/domain/repository"
 )
 
-type TodoRepository struct {
+type todoRepository struct {
 	SqlHandler
 }
 
 func NewTodoRepository(sqlHandler SqlHandler) repository.TodoRepository {
-	todoRepository := TodoRepository{sqlHandler}
+	todoRepository := todoRepository{sqlHandler}
 	return &todoRepository
 }
 
-func (tr *TodoRepository) FindAll() (*[]models.Todo, error) {
+func (tr *todoRepository) FindAll() (*[]models.Todo, error) {
 	var todos []models.Todo
 	result := tr.GetConnection().Find(&todos)
 	return &todos, result.Error
 }
 
-func (tr *TodoRepository) FindById(id uint) (*models.Todo, error) {
+func (tr *todoRepository) FindById(id uint) (*models.Todo, error) {
 	var todo models.Todo
 	result := tr.GetConnection().Where("id = ?", id).Find(&todo)
 	return &todo, result.Error
 }
 
-func (tr *TodoRepository) Create(todo *models.Todo) error {
+func (tr *todoRepository) Create(todo *models.Todo) error {
 	result := tr.GetConnection().Create(todo)
 	return result.Error
 }
 
-func (tr *TodoRepository) Update(todo *models.Todo) error {
+func (tr *todoRepository) Update(todo *models.Todo) error {
 	result := tr.GetConnection().Save(todo)
 	return result.Error
 }
 
-func (tr *TodoRepository) Delete(todo *models.Todo) error {
+func (tr *todoRepository) Delete(todo *models.Todo) error {
 	result := tr.GetConnection().Delete(todo)
 	return result.Error
 }
---
+++ app/usecases/todoUsecase.go
@@ -0,0 +1,48 @@
+package usecases
+
+import (
+	"github.com/MinadukiSekina/todo-go-app/app/domain/models"
+	"github.com/MinadukiSekina/todo-go-app/app/domain/repository"
+)
+
+type TodoUsecase interface {
+	SearchByID(uint) (*models.Todo, error)
+	Show() (todos *[]models.Todo, err error)
+	Add(todo *models.Todo) error
+	Edit(todo *models.Todo) error
+	Delete(todo *models.Todo) error
+}
+
+type todoUsecase struct {
+	repos repository.TodoRepository
+}
+
+func NewTodoUsecase(todoRepo repository.TodoRepository) TodoUsecase {
+	todoUsecase := todoUsecase{repos: todoRepo}
+	return &todoUsecase
+}
+
+func (uc *todoUsecase) SearchByID(id uint) (todo *models.Todo, err error) {
+	todo, err = uc.repos.FindById(id)
+	return
+}
+
+func (uc *todoUsecase) Show() (todos *[]models.Todo, err error) {
+	todos, err = uc.repos.FindAll()
+	return
+}
+
+func (uc *todoUsecase) Add(todo *models.Todo) (err error) {
+	err = uc.repos.Create(todo)
+	return
+}
+
+func (uc *todoUsecase) Edit(todo *models.Todo) (err error) {
+	err = uc.repos.Update(todo)
+	return
+}
+
+func (uc *todoUsecase) Delete(todo *models.Todo) (err error) {
+	err = uc.repos.Delete(todo)
+	return
+}
