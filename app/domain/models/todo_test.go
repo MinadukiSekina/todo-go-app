46e8fc29dadee3b629653f0f98bf58fd5341d611
MinadukiSekina <taroufujimoto075@gmail.com>
2025-06-13 02:41:38 +0000

    test: unify modified test case descriptions to use half-width colons

--- app/domain/models/todo_test.go
+++ app/domain/models/todo_test.go
@@ -23,43 +23,43 @@ func TestStrToStatus(t *testing.T) {
 		expectErr  bool
 		errMessage string
 	}{
-		"正常ケース：NotStarted": {
+		"正常ケース:NotStarted": {
 			args:       args{target: "NotStarted", correspond: correspond},
 			want:       NotStarted,
 			expectErr:  false,
 			errMessage: "",
 		},
-		"正常ケース：Done": {
+		"正常ケース:Done": {
 			args:       args{target: "Done", correspond: correspond},
 			want:       Done,
 			expectErr:  false,
 			errMessage: "",
 		},
-		"異常ケース：対象の文字列が空文字列": {
+		"異常ケース:対象の文字列が空文字列": {
 			args:       args{target: "", correspond: correspond},
 			want:       invalid,
 			expectErr:  true,
 			errMessage: "target string is empty",
 		},
-		"異常ケース：対象の文字列が変換マップに無い": {
+		"異常ケース:対象の文字列が変換マップに無い": {
 			args:       args{target: "test", correspond: correspond},
 			want:       invalid,
 			expectErr:  true,
 			errMessage: "invalid status value: test",
 		},
-		"異常ケース：変換マップが空": {
+		"異常ケース:変換マップが空": {
 			args:       args{target: "NotStarted", correspond: map[string]Status{}},
 			want:       invalid,
 			expectErr:  true,
 			errMessage: "correspond map is empty",
 		},
-		"異常ケース：変換後の値が列挙定数の範囲を超えている(負の数)": {
+		"異常ケース:変換後の値が列挙定数の範囲を超えている(負の数)": {
 			args:       args{target: "NotStarted", correspond: map[string]Status{"NotStarted": -1}},
 			want:       invalid,
 			expectErr:  true,
 			errMessage: "invalid status value: NotStarted",
 		},
-		"異常ケース：変換後の値が列挙定数の範囲を超えている(正の数)": {
+		"異常ケース:変換後の値が列挙定数の範囲を超えている(正の数)": {
 			args:       args{target: "NotStarted", correspond: map[string]Status{"NotStarted": 99999999999999}},
 			want:       invalid,
 			expectErr:  true,
