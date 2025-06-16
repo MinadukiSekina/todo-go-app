43370e5eb06714726ee48eaa4ee65ef88fd8eed8
MinadukiSekina <taroufujimoto075@gmail.com>
2025-06-16 09:16:33 +0000

    test: remove non-used fields

--- app/handlers/web/todoHandler_test.go
+++ app/handlers/web/todoHandler_test.go
@@ -18,6 +18,8 @@ import (
 
 func TestIndex(t *testing.T) {
 
+	gin.SetMode(gin.TestMode)
+
 	todo1 := models.Todo{Title: "test1", Status: models.NotStarted}
 	todo1.ID = 1
 
@@ -31,44 +33,37 @@ func TestIndex(t *testing.T) {
 	cases := map[string]struct {
 		prepareMockFn func(m *mock_usecases.MockTodoUsecase)
 		want          int
-		expectErr     bool
-		err           error
 	}{
 		"正常ケース:データなし": {
 			prepareMockFn: func(m *mock_usecases.MockTodoUsecase) { m.EXPECT().Show().Return(&nothingTodos, nil) },
 			want:          http.StatusOK,
-			expectErr:     false,
-			err:           nil,
 		},
 		"正常ケース:1件データあり": {
 			prepareMockFn: func(m *mock_usecases.MockTodoUsecase) { m.EXPECT().Show().Return(&onlyOneTodos, nil) },
 			want:          http.StatusOK,
-			expectErr:     false,
-			err:           nil,
 		},
 		"正常ケース:2件データあり": {
 			prepareMockFn: func(m *mock_usecases.MockTodoUsecase) { m.EXPECT().Show().Return(&manyHasTodos, nil) },
 			want:          http.StatusOK,
-			expectErr:     false,
-			err:           nil,
 		},
 		"異常ケース:エラーあり": {
 			prepareMockFn: func(m *mock_usecases.MockTodoUsecase) {
 				m.EXPECT().Show().Return(nil, errors.New("something is wrong"))
 			},
-			want:      http.StatusInternalServerError,
-			expectErr: true,
-			err:       nil,
+			want: http.StatusInternalServerError,
 		},
 	}
+
 	for name, tt := range cases {
 		t.Run(name, func(t *testing.T) {
+
 			// モックの呼び出しを管理するControllerを生成
 			mockCtrl := gomock.NewController(t)
 			defer mockCtrl.Finish()
 
 			// モックの生成
 			mock := mock_usecases.NewMockTodoUsecase(mockCtrl)
+
 			// テスト中に呼ばれるべき関数と帰り値を指定
 			tt.prepareMockFn(mock)
 
@@ -110,43 +105,38 @@ func TestShowByID(t *testing.T) {
 		prepareMockFn func(m *mock_usecases.MockTodoUsecase)
 		args          args
 		want          int
-		expectErr     bool
-		err           error
 	}{
 		"正常ケース:データあり": {
 			prepareMockFn: func(m *mock_usecases.MockTodoUsecase) { m.EXPECT().SearchByID(uint(1)).Return(&todo1, nil) },
 			args:          args{id: 1},
 			want:          http.StatusOK,
-			expectErr:     false,
-			err:           nil,
 		},
 		"異常ケース:IDを数値に変換できない": {
 			prepareMockFn: func(m *mock_usecases.MockTodoUsecase) {
 				// IDが数値に変換できない場合はSearchByIDは呼ばれない
 			},
-			args:      args{id: "string"},
-			want:      http.StatusSeeOther,
-			expectErr: true,
-			err:       nil,
+			args: args{id: "string"},
+			want: http.StatusSeeOther,
 		},
 		"異常ケース:データなし": {
 			prepareMockFn: func(m *mock_usecases.MockTodoUsecase) {
 				m.EXPECT().SearchByID(uint(1)).Return(nil, errors.New("record not found"))
 			},
-			args:      args{id: 1},
-			want:      http.StatusSeeOther,
-			expectErr: true,
-			err:       nil,
+			args: args{id: 1},
+			want: http.StatusSeeOther,
 		},
 	}
+
 	for name, tt := range cases {
 		t.Run(name, func(t *testing.T) {
+
 			// モックの呼び出しを管理するControllerを生成
 			mockCtrl := gomock.NewController(t)
 			defer mockCtrl.Finish()
 
 			// モックの生成
 			mock := mock_usecases.NewMockTodoUsecase(mockCtrl)
+
 			// テスト中に呼ばれるべき関数と帰り値を指定
 			tt.prepareMockFn(mock)
 
@@ -188,40 +178,38 @@ func TestCreate(t *testing.T) {
 		prepareMockFn func(m *mock_usecases.MockTodoUsecase)
 		args          args
 		want          int
-		expectErr     bool
-		err           error
 	}{
 		"正常ケース:作成に成功": {
 			prepareMockFn: func(m *mock_usecases.MockTodoUsecase) { m.EXPECT().Add(gomock.Any()).Return(nil) },
 			args:          args{title: "test1"},
 			want:          http.StatusFound,
-			expectErr:     false,
-			err:           nil,
 		},
 		"異常ケース:作成に失敗": {
 			prepareMockFn: func(m *mock_usecases.MockTodoUsecase) {
 				m.EXPECT().Add(gomock.Any()).Return(errors.New("create todo is failed"))
 			},
-			args:      args{title: "failed"},
-			want:      http.StatusSeeOther,
-			expectErr: true,
-			err:       nil,
+			args: args{title: "failed"},
+			want: http.StatusSeeOther,
 		},
 	}
+
 	for name, tt := range cases {
 		t.Run(name, func(t *testing.T) {
+
 			// モックの呼び出しを管理するControllerを生成
 			mockCtrl := gomock.NewController(t)
 			defer mockCtrl.Finish()
 
 			// モックの生成
 			mock := mock_usecases.NewMockTodoUsecase(mockCtrl)
+
 			// テスト中に呼ばれるべき関数と帰り値を指定
 			tt.prepareMockFn(mock)
 
 			// gin contextの生成
 			w := httptest.NewRecorder()
 			c, r := gin.CreateTestContext(w)
+
 			// テンプレートの読み込み
 			// route.goと同じ指定だとエラーになったため、appからのパスで指定する
 			r.LoadHTMLGlob("/app/templates/*/*.html")
@@ -268,71 +256,63 @@ func TestUpdate(t *testing.T) {
 		prepareMockFn func(m *mock_usecases.MockTodoUsecase)
 		args          args
 		want          int
-		expectErr     bool
-		err           error
 	}{
 		"正常ケース:更新に成功": {
 			prepareMockFn: func(m *mock_usecases.MockTodoUsecase) {
 				m.EXPECT().SearchByID(uint(1)).Return(&todo1, nil)
 				m.EXPECT().Edit(gomock.Any()).Return(nil)
 			},
-			args:      args{id: 1, title: "test1", status: "completed"},
-			want:      http.StatusFound,
-			expectErr: false,
-			err:       nil,
+			args: args{id: 1, title: "test1", status: "completed"},
+			want: http.StatusFound,
 		},
 		"異常ケース:IDが数値に変換できない": {
 			prepareMockFn: func(m *mock_usecases.MockTodoUsecase) {
 				// 変換できない場合はusecaseの処理が走る前にReturnするので何もしない
 			},
-			args:      args{id: "string", title: "failed", status: "completed"},
-			want:      http.StatusSeeOther,
-			expectErr: true,
-			err:       nil,
+			args: args{id: "string", title: "failed", status: "completed"},
+			want: http.StatusSeeOther,
 		},
 		"異常ケース:ステータスの値が変換できない": {
 			prepareMockFn: func(m *mock_usecases.MockTodoUsecase) {
 				// 変換できない場合はusecaseの処理が走る前にReturnするので何もしない
 			},
-			args:      args{id: 1, title: "failed", status: "cannotConverted"},
-			want:      http.StatusSeeOther,
-			expectErr: true,
-			err:       nil,
+			args: args{id: 1, title: "failed", status: "cannotConverted"},
+			want: http.StatusSeeOther,
 		},
 		"異常ケース:対象のタスクが存在しない": {
 			prepareMockFn: func(m *mock_usecases.MockTodoUsecase) {
 				m.EXPECT().SearchByID(uint(1)).Return(nil, errors.New("record not found"))
 			},
-			args:      args{id: 1, title: "failed", status: "completed"},
-			want:      http.StatusSeeOther,
-			expectErr: true,
-			err:       nil,
+			args: args{id: 1, title: "failed", status: "completed"},
+			want: http.StatusSeeOther,
 		},
 		"異常ケース:更新に失敗": {
 			prepareMockFn: func(m *mock_usecases.MockTodoUsecase) {
 				m.EXPECT().SearchByID(uint(1)).Return(&todo1, nil)
 				m.EXPECT().Edit(gomock.Any()).Return(errors.New("something is wrong"))
 			},
-			args:      args{id: 1, title: "failed", status: "completed"},
-			want:      http.StatusSeeOther,
-			expectErr: true,
-			err:       nil,
+			args: args{id: 1, title: "failed", status: "completed"},
+			want: http.StatusSeeOther,
 		},
 	}
+
 	for name, tt := range cases {
 		t.Run(name, func(t *testing.T) {
+
 			// モックの呼び出しを管理するControllerを生成
 			mockCtrl := gomock.NewController(t)
 			defer mockCtrl.Finish()
 
 			// モックの生成
 			mock := mock_usecases.NewMockTodoUsecase(mockCtrl)
+
 			// テスト中に呼ばれるべき関数と帰り値を指定
 			tt.prepareMockFn(mock)
 
 			// gin contextの生成
 			w := httptest.NewRecorder()
 			c, r := gin.CreateTestContext(w)
+
 			// テンプレートの読み込み
 			// route.goと同じ指定だとエラーになったため、appからのパスで指定する
 			r.LoadHTMLGlob("/app/templates/*/*.html")
@@ -381,39 +361,33 @@ func TestDelete(t *testing.T) {
 		prepareMockFn func(m *mock_usecases.MockTodoUsecase)
 		args          args
 		want          int
-		expectErr     bool
-		err           error
 	}{
 		"正常ケース:削除に成功": {
 			prepareMockFn: func(m *mock_usecases.MockTodoUsecase) {
 				m.EXPECT().Delete(uint(1)).Return(nil)
 			},
-			args:      args{id: 1},
-			want:      http.StatusFound,
-			expectErr: false,
-			err:       nil,
+			args: args{id: 1},
+			want: http.StatusFound,
 		},
 		"異常ケース:IDが数値に変換できない": {
 			prepareMockFn: func(m *mock_usecases.MockTodoUsecase) {
 				// 変換できない場合はusecaseの処理が走る前にReturnするので何もしない
 			},
-			args:      args{id: "string"},
-			want:      http.StatusSeeOther,
-			expectErr: true,
-			err:       nil,
+			args: args{id: "string"},
+			want: http.StatusSeeOther,
 		},
 		"異常ケース:更新に失敗": {
 			prepareMockFn: func(m *mock_usecases.MockTodoUsecase) {
 				m.EXPECT().Delete(uint(1)).Return(errors.New("something is wrong"))
 			},
-			args:      args{id: 1},
-			want:      http.StatusSeeOther,
-			expectErr: true,
-			err:       nil,
+			args: args{id: 1},
+			want: http.StatusSeeOther,
 		},
 	}
+
 	for name, tt := range cases {
 		t.Run(name, func(t *testing.T) {
+
 			// モックの呼び出しを管理するControllerを生成
 			mockCtrl := gomock.NewController(t)
 			defer mockCtrl.Finish()
