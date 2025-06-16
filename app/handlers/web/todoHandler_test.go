44ae62b702dd8504abd6f6c612f239b0344fa8ab
MinadukiSekina <taroufujimoto075@gmail.com>
2025-06-16 09:04:39 +0000

    test: add unit tests for Update method in TodoHandler with normal and error cases

--- app/handlers/web/todoHandler_test.go
+++ app/handlers/web/todoHandler_test.go
@@ -249,3 +249,118 @@ func TestCreate(t *testing.T) {
 		})
 	}
 }
+
+func TestUpdate(t *testing.T) {
+
+	gin.SetMode(gin.TestMode)
+
+	todo1 := models.Todo{Title: "test1", Status: models.NotStarted}
+	todo1.ID = 1
+
+	// テスト用の引数を格納する
+	type args struct {
+		id     any
+		title  string
+		status string
+	}
+
+	cases := map[string]struct {
+		prepareMockFn func(m *mock_usecases.MockTodoUsecase)
+		args          args
+		want          int
+		expectErr     bool
+		err           error
+	}{
+		"正常ケース:更新に成功": {
+			prepareMockFn: func(m *mock_usecases.MockTodoUsecase) {
+				m.EXPECT().SearchByID(uint(1)).Return(&todo1, nil)
+				m.EXPECT().Edit(gomock.Any()).Return(nil)
+			},
+			args:      args{id: 1, title: "test1", status: "completed"},
+			want:      http.StatusFound,
+			expectErr: false,
+			err:       nil,
+		},
+		"異常ケース:IDが数値に変換できない": {
+			prepareMockFn: func(m *mock_usecases.MockTodoUsecase) {
+				// 変換できない場合はusecaseの処理が走る前にReturnするので何もしない
+			},
+			args:      args{id: "string", title: "failed", status: "completed"},
+			want:      http.StatusSeeOther,
+			expectErr: true,
+			err:       nil,
+		},
+		"異常ケース:ステータスの値が変換できない": {
+			prepareMockFn: func(m *mock_usecases.MockTodoUsecase) {
+				// 変換できない場合はusecaseの処理が走る前にReturnするので何もしない
+			},
+			args:      args{id: 1, title: "failed", status: "cannotConverted"},
+			want:      http.StatusSeeOther,
+			expectErr: true,
+			err:       nil,
+		},
+		"異常ケース:対象のタスクが存在しない": {
+			prepareMockFn: func(m *mock_usecases.MockTodoUsecase) {
+				m.EXPECT().SearchByID(uint(1)).Return(nil, errors.New("record not found"))
+			},
+			args:      args{id: 1, title: "failed", status: "completed"},
+			want:      http.StatusSeeOther,
+			expectErr: true,
+			err:       nil,
+		},
+		"異常ケース:更新に失敗": {
+			prepareMockFn: func(m *mock_usecases.MockTodoUsecase) {
+				m.EXPECT().SearchByID(uint(1)).Return(&todo1, nil)
+				m.EXPECT().Edit(gomock.Any()).Return(errors.New("something is wrong"))
+			},
+			args:      args{id: 1, title: "failed", status: "completed"},
+			want:      http.StatusSeeOther,
+			expectErr: true,
+			err:       nil,
+		},
+	}
+	for name, tt := range cases {
+		t.Run(name, func(t *testing.T) {
+			// モックの呼び出しを管理するControllerを生成
+			mockCtrl := gomock.NewController(t)
+			defer mockCtrl.Finish()
+
+			// モックの生成
+			mock := mock_usecases.NewMockTodoUsecase(mockCtrl)
+			// テスト中に呼ばれるべき関数と帰り値を指定
+			tt.prepareMockFn(mock)
+
+			// gin contextの生成
+			w := httptest.NewRecorder()
+			c, r := gin.CreateTestContext(w)
+			// テンプレートの読み込み
+			// route.goと同じ指定だとエラーになったため、appからのパスで指定する
+			r.LoadHTMLGlob("/app/templates/*/*.html")
+
+			// フォームデータの組み立て
+			formData := url.Values{}
+			formData.Add("title", tt.args.title)
+			formData.Add("status", tt.args.status)
+
+			// リクエストを設定
+			req, _ := http.NewRequest("POST", fmt.Sprintf("/todo/%v", tt.args.id), strings.NewReader(formData.Encode()))
+			req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+			c.Request = req
+
+			// パラメータを設定
+			c.Params = []gin.Param{{Key: "id", Value: fmt.Sprint(tt.args.id)}}
+
+			// mockを利用してテストする
+			handler := NewTodoHandler(mock)
+			handler.Update(c)
+
+			// GETの場合と異なり、POSTの場合はリダイレクトのステータスコードが書き込まれないらしい
+			// 回避策として、c.Redirectの後に明示的に書き込むようにする（Create内部でc.Redirectを呼んでいる）
+			// ref: https://stackoverflow.com/questions/76319196/unit-testing-of-gins-context-redirect-works-for-get-response-code-but-fails-for
+			c.Writer.WriteHeaderNow()
+
+			// 結果を確認
+			assert.Equal(t, tt.want, w.Code)
+		})
+	}
+}
